217e5b5bd060aa6bbbcc944f09a09d6d4c18e58b
mmirry <mumbuamirry20@gmail.com>
2023-07-13 14:04:43 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main- Using puts function program to print a line
+ * Return:0
+ */
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle");
+
+	return 0;
+}
