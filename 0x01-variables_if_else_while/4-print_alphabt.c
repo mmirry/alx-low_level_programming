9394b3c179bc347773b90f0a535256bff5687b4c
mmirry <mumbuamirry20@gmail.com>
2023-07-16 12:28:31 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,9 +8,12 @@ int main(void)
 char c;
 c = 'a';
 while
-((c != 'q' && c != 'e') && c <= 'z')
+(c <= 'z')	
+{
+if ((c != 'q' && c != 'e') && c <= 'z')
 putchar(c);
 c++;
+}
 putchar('\n');
 return (0);
 }
