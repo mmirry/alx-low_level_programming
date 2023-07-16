cf7a61601bb3644bb8a121f1111ee1cf3d199331
mmirry <mumbuamirry20@gmail.com>
2023-07-16 04:53:01 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdio.h>
+/**
+* main - Program that prints alphabet in lowercase.
+* Return: (0)
+*/
+int main(void)
+{
+char c;
+c = 'a';
+while
+(c <= 'z');
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
+
