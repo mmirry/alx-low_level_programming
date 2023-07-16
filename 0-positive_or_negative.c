5a12ad2490ff8b14a9c880cc82059fa4c3cbb336
mmirry <mumbuamirry20@gmail.com>
2023-07-15 23:24:09 -0700

    0-positive_or_negative.c

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -2,28 +2,26 @@
 #include<time.h>
 #include<stdio.h>
 /**
-* main - A program to determine if a line is positive, negative or zero.
+* main - Program that prints a line with positive, negative or zero.
 * Return: (0)
 */
 int main(void)
 {
 int n;
 srand(time(0));
-n - rand() - RAND_MAX / 2;
-
-if (n > 0)
-{
+n = rand() - RAND_MAX / 2;
+if (n > 0)	
+{	
 printf("%d is positive\n", n);
 }
-else if (n == 0)
-{
-printf("%d is zero\n", n);
-}
-else if (n < 0)	
+if else (n < 0)
 {
 printf("%d is negative\n", n);
 }
+else (n == 0)
+{
+printf("%d is zero\n", n);
+}
 return (0);
 }
-
 
